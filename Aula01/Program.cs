8b5059da85a3377c5b24b5672293b17d3a503f28
João <sj.joaopedro@gmail.com>
2026-08-13 20:45:32 -0300

    feat: Revisão da Lógica

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string nomeCliente = "João";
+int idadeCliente = 18;
+double valorBike = 2000.00;
+string modeloBike = "Caloi";
+bool clienteAtivo = true;
+
+Console.WriteLine("LOJA DE BIKE");
+Console.WriteLine($"Nome do Cliente: {nomeCliente}");
+Console.WriteLine($"Idade do Cliente: {idadeCliente}");
+Console.WriteLine($"Valor da Bike: {valorBike}");
+Console.WriteLine($"Modelo Da Bike: {modeloBike}");
+Console.WriteLine($"Cliente Ativo: {clienteAtivo}");
+
+
+
+
+
+//int anosAutonomia = 450;
+//double capacidadeBateria = 75.5;
+//string modeloVeiculo = "ByD";
+//bool carregamentoRapido = true;
+
+//Console.WriteLine($"Veiculo: {modeloVeiculo}");
+//Console.WriteLine($"Autonomia: {anosAutonomia} km | Bateria: {capacidadeBateria} KWh");
+//Console.WriteLine($"Possui carregamento rapido: {carregamentoRapido}");
+
+////Verificar quanto pagar de imposto ao comprar um carro Eletrico
+
+//const double juros = 0.10; // 10%
+//double precoVeiculo = 20000.00;
+
+//double valorImposto = precoVeiculo * juros;
+//Console.WriteLine($"O valor do iomposto é R$ {valorImposto:N2}");
